1a3f79f11c2660be3ff783de968843d6d2143ad9
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-07 04:08:28 -0700

    Updated routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import addMahasiswa from "addMahasiswa.js";
-import editMahasiswa from "editMahasiswa.js";
+import addMahasiswa from "./addMahasiswa.js";
+import editMahasiswa from "./editMahasiswa.js";
+import deleteMahasiswa from "./deleteMahasiswa.js";
 
 const route = Router();
 route.post("/add", addMahasiswa);
 route.put("/edit/:id", editMahasiswa);
+route.delete("/delete/:id", deleteMahasiswa);
 
 export default route;
