accf5b988496f2c28ad419a5fdf8f5939582a464
AdamRifandy <adamrifandy123@gmail.com>
2026-05-07 16:56:49 +0700

    Updated routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import addMahasiswa from "addMahasiswa.js";
+import editMahasiswa from "editMahasiswa.js";
 
 const route = Router();
+route.post("/mahasiswa/add", addMahasiswa);
+route.put("/mahasiswa/edit/:id", editMahasiswa);
 
 export default route;
