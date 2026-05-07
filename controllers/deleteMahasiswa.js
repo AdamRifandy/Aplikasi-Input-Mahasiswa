71d46f4032ed6751b2e5d326c841e3d7893f3eb8
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-07 04:02:21 -0700

    Added deleteMahasiswa.js

---
+++ controllers/deleteMahasiswa.js
@@ -0,0 +1,24 @@
+import db from "../connection.js";
+
+export default (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const sql = conn.prepare("DELETE FROM mahasiswa WHERE id_mahasiswa = ?").run(id);
+    if (!sql) return res.status(400).json({
+      message: "Gagal menghapus data mahasiswa!",
+      data: sql,
+      status: false
+    });
+    return res.json({
+      message: "Berhasil menghapus data mahasiswa.",
+      data: sql,
+      status: true
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+      data: err,
+      status: false
+    });
+  }
+};
