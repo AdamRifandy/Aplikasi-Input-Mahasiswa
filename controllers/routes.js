89f8a7dbafbbe114c558c7b7504b301f076b0264
AdamRifandy <adamrifandy123@gmail.com>
2026-05-08 16:18:22 +0700

    Updated routes.js

--- controllers/routes.js
+++ controllers/routes.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 import addMahasiswa from "./addMahasiswa.js";
 import editMahasiswa from "./editMahasiswa.js";
 import deleteMahasiswa from "./deleteMahasiswa.js";
+import getMahasiswa from "./getMahasiswa.js";
+import allMahasiswa from "./allMahasiswa.js";
 
 const route = Router();
 route.post("/add", addMahasiswa);
 route.put("/edit/:id", editMahasiswa);
 route.delete("/delete/:id", deleteMahasiswa);
+route.post("/get/:id", getMahasiswa);
+route.get("/all", allMahasiswa);
 
 export default route;
