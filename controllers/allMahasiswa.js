306feb6c0fea437ef70c52f89013619da1554849
AdamRifandy <adamrifandy123@gmail.com>
2026-05-08 16:14:11 +0700

    Updated allMahasiswa.js

--- controllers/allMahasiswa.js
+++ controllers/allMahasiswa.js
@@ -1,2 +1,22 @@
 import conn from '../db/connection.js';
-export default (req, res) => {};
+export default (req, res) => {
+  try {
+    const prepare = conn.prepare("SELECT * FROM mahasiswa");
+    if (!prepare) return res.status(500).json({
+      message: "Gagal mendapatkan semua data mahasiswa",
+      data: prepare,
+      status: false
+    });
+    return res.json({
+      message: "Berhasil mendapatkan semua data mahasiswa",
+      data: prepare,
+      status: true
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+      data: err,
+      status: false
+    })
+  }
+};
