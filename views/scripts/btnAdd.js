27d1ec5693079d39ff58a150fce6e3336b0dae5a
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-09 07:38:34 -0700

    Updated btnAdd.js

--- views/scripts/btnAdd.js
+++ views/scripts/btnAdd.js
@@ -1,4 +1,4 @@
 document.getElementById("btnAdd").addEventListener('click', (e) => {
   e.preventDefault();
-  window.location.href = "../add_edit.html?type='Tambah'";
+  window.location.href = 1../add_edit.html?type="Tambah"`;
 });
