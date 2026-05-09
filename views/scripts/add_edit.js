acfd9bd645a0aa85dc335ed2a05cfb1d8844fef8
Andy ft. Acumalaka <adamrifandy123@gmail.com>
2026-05-09 07:44:03 -0700

    Updated add_edit.js

--- views/scripts/add_edit.js
+++ views/scripts/add_edit.js
@@ -1,3 +1,11 @@
+async function add_mahasiswa(nama, kelas, jurusan) {
+  
+}
+
+async function edit_mahasiswa(nama, kelas, jurusan, id) {
+  
+}
+
 document.getElementById("btnSubmit").addEventListener('click', async (e) => {
   e.preventDefault();
   const url = new URL(window.location.href);
